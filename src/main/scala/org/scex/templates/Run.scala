0df1d0c395dc745c577df1ea5bb414b26eabbe88
Jentsch <d.jentsch@fu-berlin.de>
2014-01-17 00:29:42 +0100

    Slim Run.scala

--- src/main/scala/org/scex/templates/Run.scala
+++ src/main/scala/org/scex/templates/Run.scala
@@ -5,22 +5,14 @@ import org.scex._
 /**
  * A document annotated with this trait will be runnable.
  */
-trait Run extends General {
-  
-  private var bodies = List.empty[() => Unit] 
-  
-  def delayedInit(body: => Unit) = {
-    bodies = {() => body } :: bodies
-  }
-  
-  def main(params: Array[String]) {
-    bodies foreach {
-      _ ()
-    }
-    
+trait Run extends General with App {
+
+  override def main(params: Array[String]) {
+    super.main(params)
+
     scala.xml.XML.save(filename, generators HTML this, "UTF-8")
 
-    println("Documente saved under " + filename)
+    println("Document saved under " + filename)
   }
 
   private def filename = name + ".html"
