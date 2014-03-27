e3abe848a89d5ce16f2926d35d56f695a187f472
Jentsch <d.jentsch@fu-berlin.de>
2014-03-27 13:42:54 +0100

    Improve documentation

--- src/main/scala/org/scex/attributes/Breaks.scala
+++ src/main/scala/org/scex/attributes/Breaks.scala
@@ -2,13 +2,16 @@ package org.scex.attributes
 
 trait Breaks {
   sealed class Break(val name: String)
-  
+ 
+  /**
+   * Neighter deny a page brake or force it.
+   */
   val auto = new Break("auto")
   object page extends Break("page") {
     val odd = new Break("odd-page")
     val even = new Break("even-page")
   }
-  val column = new Break("coluomn")
+  val column = new Break("column")
   
   val BreakBefore = new Attribute[Break]("BreakBefore")
   val BreakAfter  = new Attribute[Break]("BreakAfter")
