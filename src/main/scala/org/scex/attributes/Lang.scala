b13132a4fa3ea670def1ae57a086212023b221a9
Jentsch <d.jentsch@fu-berlin.de>
2014-03-27 23:04:32 +0100

    Add shortcut for plural

--- src/main/scala/org/scex/attributes/Lang.scala
+++ src/main/scala/org/scex/attributes/Lang.scala
@@ -1,16 +1,28 @@
 package org.scex.attributes
 
 sealed trait Lang {
+  import Lang._
+
   def hypenOf(word: String): String = hyphen(word)
 
   private val hyphen: Map[String, String] =
     io.Source.fromInputStream(getClass.getResourceAsStream(toString + ".dic")).
     getLines.
     map(_.replace("-", "\u00AD")).
+    flatMap{ word =>
+      if (word contains '(')
+        Seq(word takeWhile (_ != '('), word filterNot Bracket)
+      else
+        Seq(word)
+    }.
     toTraversable.
     groupBy(_.replace("\u00AD", "")).
     mapValues(_.head).
     withDefault(identity)
+}
+
+private object Lang {
+  val Bracket = Set('(', ')')
 }
 
 case object de extends Lang
