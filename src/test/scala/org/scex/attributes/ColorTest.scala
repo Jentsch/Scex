2e79f57162054bf7af4f98d4fb52c6e1ca44280b
Jentsch <d.jentsch@fu-berlin.de>
2015-03-27 21:50:35 +0100

    Added tests for colors and removed unused methods

---
+++ src/test/scala/org/scex/attributes/ColorTest.scala
@@ -0,0 +1,13 @@
+package org.scex.attributes
+
+import org.specs2.Specification
+
+class ColorTest extends Specification {
+  def is = s2"""
+  ${"Color".title}C
+  ${red.toHex must be equalTo "FF0000"}
+  ${blue.toHex must be equalTo "0000FF"}
+  ${green.toHex must be equalTo "008000"}
+  ${rgb(17, 33, 65).toHex must be equalTo "112141"}
+  """
+}
